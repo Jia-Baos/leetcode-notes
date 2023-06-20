6fb57b91ac8e763916271b02d3faae89f17974b2
Jia-Baos <18383827268@163.com>
2023-06-20 23:37:50 +0800

    2023-06-20 23:37

--- main.cpp
+++ main.cpp
@@ -1,44 +1,27 @@
 #include <bits/stdc++.h>
 
-/*每一层递归结束后，计算得到的有序序列会赋值给arr*/
-void MergeSortRecursive(std::array<int, 8> &arr, const int left, const int right)
-{
-    std::array<int, 8> temp = arr;
-
-    if (left >= right)
-    {
-        return;
-    }
-
-    int N = right - left;     // 7
-    int mid = (N / 2) + left; // 3
-    int start1 = left;        // 0
-    int end1 = mid;           // 3
-    int start2 = mid + 1;     // 4
-    int end2 = right;         // 7
-
-    MergeSortRecursive(temp, start1, end1);
-    MergeSortRecursive(temp, start2, end2);
-
-    int k = left;
-    while (start1 <= end1 && start2 <= end2)
-    {
-        arr[k++] = temp[start1] < temp[start2] ? temp[start1++] : temp[start2++];
-    }
-    while (start1 <= end1)
-    {
-        arr[k++] = temp[start1++];
-    }
-    while (start2 <= end2)
-    {
-        arr[k++] = temp[start2++];
-    }
-}
-
 int main(int argc, char *argv[])
 {
     std::array<int, 8> arr = {6, 2, 1, 3, 7, 5, 4, 8};
-    MergeSortRecursive(arr, 0, arr.size() - 1);
+
+    int gap = arr.size() / 2;
+
+    while (gap > 0)
+    {
+        for (size_t i = gap; i < arr.size(); i++)
+        {
+            int temp = arr[i];
+            int j = i;
+            while (j >= gap && arr[j - gap] > temp)
+            {
+                arr[j] = arr[j - gap];
+                j -= gap;
+            }
+            arr[j] = temp;
+        }
+
+        gap /= 2;
+    }
 
     for (auto &iter : arr)
     {
