db19e43ee4a2cc5be357e53a576134951affe3ed
Jia-Baos <18383827268@163.com>
2023-06-26 11:36:32 +0800

    2023-06-26 11:36

--- main.cpp
+++ main.cpp
@@ -1,48 +1,18 @@
 #include <bits/stdc++.h>
 
-int Solution(std::array<int, 4> &arr)
-{
-    int k = 3;
-    int t = 0;
-
-    std::map<int, int> window;
-    int left = 0;
-    int right = 0;
-
-    while (right < arr.size())
-    {
-        window.insert({arr[right], right});
-
-        if (right - left > k)
-        {
-            window.erase(arr[left]);
-            left += 1;
-        }
-
-        int idx = arr[right];
-        if (idx > 0 && std::abs(window[idx] - window[idx - 1]) <= t)
-        {
-            return 0;
-        }
-
-        if (idx < arr.size() - 1 && std::abs(window[idx + 1] - window[idx]) <= t)
-        {
-            return 0;
-        }
-
-        right += 1;
-    }
-
-    return -1;
-}
-
 int main(int argc, char *argv[])
 {
-    std::array<int, 4> arr = {1, 2, 3, 1};
+    std::string str = "Hello";
 
-    int dst = Solution(arr);
+    for (auto &iter : str)
+    {
+        if (std::isupper(iter))
+        {
+            iter = std::tolower(iter);
+        }
+    }
 
-    std::cout << "dst: " << dst << std::endl;
+    std::cout << str << std::endl;
 
     return 0;
 }
