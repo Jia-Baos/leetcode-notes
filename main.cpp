d88190514e6c39f2dcbd7883e5121dc4d4f28c5f
Jia-Baos <18383827268@163.com>
2023-06-26 22:19:14 +0800

    2023-06-26 22:19

--- main.cpp
+++ main.cpp
@@ -1,28 +1,16 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> 
 
-int Solution(std::vector<int> &vec, std::vector<int> &temp)
+int Solution(std::vector<int> &vec)
 {
-    for (size_t i = 1; i < vec.size(); i++)
-    {
-        int j = i;
-        int temp = vec[i];
 
-        while (j > 0 && vec[j - 1] > temp)
-        {
-            vec[j] = vec[j - 1];
-            j -= 1;
-        }
-
-        vec[j] = temp;
-    }
     return 0;
 }
 
 int main(int argc, char *argv[])
 {
     std::vector<int> vec = {6, 2, 3, 5, 1, 4};
-    std::vector<int> temp;
-    int ans = Solution(vec, temp);
+
+    int ans = Solution(vec);
 
     std::cout << "ans: " << ans << std::endl;
 
