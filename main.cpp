f8ef7dfba7c6190ce87e18baf7a216ce8549e90e
Jia-Baos <18383827268@163.com>
2023-07-09 23:14:53 +0800

    2023-07-09 23:14

--- main.cpp
+++ main.cpp
@@ -1,58 +1,51 @@
 #include <bits/stdc++.h>
 
-struct RandomListNode
+struct ListNode
 {
     int val_;
-    RandomListNode *next_;
-    RandomListNode *random_;
-    RandomListNode(int val) : val_(val),
-                              next_(nullptr),
-                              random_(nullptr) {}
+    ListNode *next_;
+    ListNode(int val) : val_(val), next_(nullptr) {}
 };
 
-class Solution
+int Solution(ListNode *head)
 {
-public:
-    std::unordered_map<RandomListNode *, RandomListNode *> cachedNode;
-
-    RandomListNode *copyRandomList(RandomListNode *head)
+    int flag = 1;
+    std::set<ListNode *> node_set;
+    ListNode *iter = head;
+    while (iter)
     {
-        if (head == nullptr)
+        if (node_set.find(iter) != node_set.end())
         {
-            return nullptr;
+            flag = 0;
+            break;
         }
-        if (!cachedNode.count(head))
+        else
         {
-            RandomListNode *headNew = new RandomListNode(head->val_);
-            cachedNode[head] = headNew;
-            headNew->next_ = copyRandomList(head->next_);
-            headNew->random_ = copyRandomList(head->random_);
+            node_set.insert(iter);
+            iter = iter->next_;
         }
-        return cachedNode[head];
     }
-};
+
+    std::cout << "The first node's value: " << iter->val_ << std::endl;
+
+    return flag;
+}
 
 int main(int argc, char *argv[])
 {
 
-    RandomListNode *listnode0 = new RandomListNode(7);
-    RandomListNode *listnode1 = new RandomListNode(13);
-    RandomListNode *listnode2 = new RandomListNode(11);
-    RandomListNode *listnode3 = new RandomListNode(10);
-    RandomListNode *listnode4 = new RandomListNode(1);
+    ListNode *listnode0 = new ListNode(3);
+    ListNode *listnode1 = new ListNode(2);
+    ListNode *listnode2 = new ListNode(0);
+    ListNode *listnode3 = new ListNode(4);
     listnode0->next_ = listnode1;
-    listnode0->random_ = nullptr;
     listnode1->next_ = listnode2;
-    listnode1->random_ = listnode0;
     listnode2->next_ = listnode3;
-    listnode2->random_ = listnode4;
-    listnode3->next_ = listnode4;
-    listnode3->random_ = listnode2;
-    listnode4->next_ = nullptr;
-    listnode4->random_ = listnode0;
+    listnode3->next_ = listnode1;
 
-    Solution *solution = new Solution();
-    RandomListNode *listnode = solution->copyRandomList(listnode0);
+    int val = Solution(listnode0);
+
+    std::cout << val << std::endl;
 
     return 0;
 }
